6aa955e3e5b635a56f2303d7f80b2c6967bbaa82
Rick South <rick@natamus.com>
2024-07-28 07:34:15 +0200

    (v3.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/extractpoison/ModFabric.java
+++ Fabric/src/main/java/com/natamus/extractpoison/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.extractpoison;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.extractpoison.events.PoisonEvent;
 import com.natamus.extractpoison.util.Reference;
 import net.fabricmc.api.ModInitializer;
@@ -11,6 +12,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
